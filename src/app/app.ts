8f406ac661be3a4a274e2bd1fc2b84115e13af80
Christoph <c.wagner.germany@gmail.com>
2025-07-21 17:24:53 +0200

    app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,27 @@
 import { Component, signal } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
+    <mat-toolbar>My Contatcfs</mat-toolbar>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+    mat-toolbar {
+      justify-content: space-between;
+
+      @include mat.toolbar-overrides (
+        (
+          container-background-color: var(--mat-sys-primary),
+          container-text-color: var(--mat-sys-on-primary),
+        )
+      );
+    }
+    `],
 })
 export class App {
   protected readonly title = signal('angular-crud');
