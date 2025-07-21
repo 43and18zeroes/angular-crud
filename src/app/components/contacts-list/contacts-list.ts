6a954f1f6b1613a231902ae580ee887d14ede012
Christoph <c.wagner.germany@gmail.com>
2025-07-21 17:30:49 +0200

    contact

---
+++ src/app/components/contacts-list/contacts-list.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacts-list',
+  imports: [],
+  template: `
+    <p>
+      contacts-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class ContactsList {
+
+}
