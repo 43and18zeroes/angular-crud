467ee9da702a77e1bfc78768ced6f8c27882e500
Christoph <c.wagner.germany@gmail.com>
2025-07-22 16:35:13 +0200

    del

--- src/app/components/contacts-list/contacts-list.ts
+++ src/app/components/contacts-list/contacts-list.ts
@@ -3,16 +3,23 @@ import { Contact } from '../../model/contact';
 import { MatListModule } from '@angular/material/list';
 import { ApiService } from '../../services/api-service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-contacts-list',
-  imports: [MatListModule, MatProgressSpinnerModule],
+  imports: [MatListModule, MatProgressSpinnerModule, MatIconModule, MatButtonModule],
   template: `
     <mat-list>
       @for (contact of contactsResource.value(); track contact.id) {
       <mat-list-item>
         <h3 matListItemTitle>{{ contact.name }}</h3>
         <p matListItemLine>{{ contact.email }}</p>
+        <div matListItemMeta>
+          <button mat-icon-button>
+            <mat-icon>delete</mat-icon>
+          </button>
+        </div>
       </mat-list-item>
       }
     </mat-list>
