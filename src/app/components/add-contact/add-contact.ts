aaf30a3909e8dbe52b127f30231c6b7c0dc6b63f
Christoph <c.wagner.germany@gmail.com>
2025-07-23 17:59:28 +0200

    buttons

--- src/app/components/add-contact/add-contact.ts
+++ src/app/components/add-contact/add-contact.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [MatFormFieldModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule],
   template: `
-    <h2>Add contact</h2>
-    <form class="fields">
-      <mat-form-field>
-        <mat-label>Name</mat-label>
-        <input matInput />
-      </mat-form-field>
-      <mat-form-field>
-        <mat-label>Email</mat-label>
-        <input matInput />
-      </mat-form-field>
-      <mat-form-field>
-        <mat-label>Phone</mat-label>
-        <input matInput />
-      </mat-form-field>
-      <div>
+    <form>
+      <h2>Add contact</h2>
+      <div class="fields">
+        <mat-form-field>
+          <mat-label>Name</mat-label>
+          <input matInput />
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Email</mat-label>
+          <input matInput />
+        </mat-form-field>
+        <mat-form-field>
+          <mat-label>Phone</mat-label>
+          <input matInput />
+        </mat-form-field>
+      </div>
+      <div class="actions">
         <button mat-flat-button>Save</button>
         <button mat-raised-button>Cancel</button>
       </div>
@@ -37,8 +40,6 @@ import { MatInputModule } from '@angular/material/input';
       grid-template-columns: repeat(2, 1fr);
       gap: 16px;
     }
-  `
+  `,
 })
-export class AddContact {
-
-}
+export class AddContact {}
