e9b70e231ac0df5191d4120d715f981853d3dd2b
Christoph <c.wagner.germany@gmail.com>
2025-07-24 16:07:47 +0200

    save

--- src/app/components/add-contact/add-contact.ts
+++ src/app/components/add-contact/add-contact.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,27 +7,27 @@ import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-contact',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink, FormsModule],
   template: `
-    <form>
+    <form (ngSubmit)="save()">
       <h2>Add contact</h2>
       <div class="fields">
         <mat-form-field>
           <mat-label>Name</mat-label>
-          <input matInput />
+          <input matInput [(ngModel)]="name" name="name" />
         </mat-form-field>
         <mat-form-field>
           <mat-label>Email</mat-label>
-          <input matInput />
+          <input matInput [(ngModel)]="email" name="email" />
         </mat-form-field>
         <mat-form-field>
           <mat-label>Phone</mat-label>
-          <input matInput />
+          <input matInput [(ngModel)]="phone" name="phone" />
         </mat-form-field>
       </div>
       <div class="actions">
-        <button mat-flat-button>Save</button>
-        <button mat-raised-button routerLink="/">Cancel</button>
+        <button type="submit" mat-flat-button>Save</button>
+        <button type="button" mat-raised-button routerLink="/">Cancel</button>
       </div>
     </form>
   `,
@@ -48,4 +49,13 @@ import { RouterLink } from '@angular/router';
     }
   `,
 })
-export class AddContact {}
+export class AddContact {
+
+  name = signal('');
+  email = signal('');
+  phone = signal('');
+
+  save() {
+    console.log(this.name(), this.email(), this.phone());
+  }
+}
