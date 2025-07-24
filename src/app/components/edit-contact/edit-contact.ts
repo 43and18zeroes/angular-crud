db748b9e8d71e881695d2f30b9b0346584124a07
Christoph <c.wagner.germany@gmail.com>
2025-07-24 16:45:27 +0200

    coalescing

--- src/app/components/edit-contact/edit-contact.ts
+++ src/app/components/edit-contact/edit-contact.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, resource } from '@angular/core';
+import { ApiService } from '../../services/api-service';
 
 @Component({
   selector: 'app-edit-contact',
   imports: [],
   template: `
     <p>
-      edit-contact works!
+      {{ contactResource.value()?.name }}
     </p>
   `,
   styles: ``
 })
 export class EditContact {
+  id = input<string>();
 
+  apiService = inject(ApiService)
+
+  contactResource = resource({
+    request: this.id,
+    loader: ({ request: id }) => this.apiService.getContact(id),
+  })
 }
